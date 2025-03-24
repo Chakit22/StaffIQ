e67cdaab60a68030230117e6383110c194f12516
Chakit Bhandari <joshua@filmneverdie.com>
2025-03-24 23:38:58 +1100

    fix: change category names

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -1,15 +1,15 @@
 import { CaptchaImage } from "@/types/CaptchaImage";
 
 export const images: CaptchaImage[] = [
-  { category: "dog", path: "/images/dog1.jpg" },
-  { category: "dog", path: "/images/dog2.jpg" },
-  { category: "dog", path: "/images/dog3.jpg" },
-  { category: "cat", path: "/images/cat1.jpg" },
-  { category: "cat", path: "/images/cat2.jpg" },
-  { category: "elephant", path: "/images/elephant1.jpg" },
-  { category: "elephant", path: "/images/elephant2.jpg" },
-  { category: "bird", path: "/images/bird1.jpg" },
-  { category: "bird", path: "/images/bird2.jpg" },
+  { category: "dogs", path: "/images/dog1.jpg" },
+  { category: "dogs", path: "/images/dog2.jpg" },
+  { category: "dogs", path: "/images/dog3.jpg" },
+  { category: "cats", path: "/images/cat1.jpg" },
+  { category: "cats", path: "/images/cat2.jpg" },
+  { category: "elephants", path: "/images/elephant1.jpg" },
+  { category: "elephants", path: "/images/elephant2.jpg" },
+  { category: "birds", path: "/images/bird1.jpg" },
+  { category: "birds", path: "/images/bird2.jpg" },
 ];
 
 export const captchaCategories: string[] = [
