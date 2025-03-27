17800e413ed7c4dd1696b1b78cca0d288fc38d8a
Chakit Bhandari <joshua@filmneverdie.com>
2025-03-27 22:41:12 +1100

    fix: Setup lecturer page

--- src/pages/lecturer.tsx
+++ src/pages/lecturer.tsx
@@ -1,22 +1,67 @@
 "use client";
 
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Tutorformtype } from "@/types/TutorFormtype";
 import { useAuth } from "@/context/UserProvider";
+import { Label } from "@radix-ui/react-label";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { courses } from "@/utils/courses";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { roles } from "@/utils/roles";
+import { availability } from "@/utils/availbility";
 
-export default function Lecturer() {
+export default function Tutor() {
   const { user } = useAuth();
   const router = useRouter();
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<Tutorformtype>();
+
+  const onSubmit = (formData: Tutorformtype) => {
+    console.log(formData);
+    toast.success("Application submitted sucessfully!");
+    // Reset the form
+    reset();
+  };
+
   useEffect(() => {
+    console.log(user);
     if (!user) {
       router.replace("/");
     }
-  }, [user]);
+  }, []);
 
   return (
-    <div className="text-black text-2xl w-full flex justify-center items-center min-h-screen">
-      Lecturers
-    </div>
+    <>
+      <Navbar />
+      {/* Main content */}
+      <Card></Card>
+      <Footer />
+    </>
   );
 }
--- src/utils/default-users.ts
+++ src/utils/default-users.ts
@@ -9,4 +9,12 @@ export const DEFAULT_USERS: User[] = [
     password: "Chuck@123",
     role: "tutor",
   },
+  {
+    firstname: "chakit",
+    lastname: "bhandari",
+    phone: 434057320,
+    email: "chakitbhandari93@gmail.com",
+    password: "Chuck@123",
+    role: "lecturer",
+  },
 ];
