393d1fa50feeb74d258160898e3b93fb97429856
SrijaRamanan711 <s4058437@student.rmit.edu.au>
2025-04-13 16:27:59 +1000

    style: layout refinements and inline comment updates in lecturer and tutor pages

--- src/pages/tutor.tsx
+++ src/pages/tutor.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import React from "react";
-
+import React, { useEffect } from "react";
 import { useAuth } from "@/context/UserProvider";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 import Layout from "@/components/layout";
 import TutorComponent from "@/components/tutor";
 
@@ -13,15 +11,24 @@ export default function Tutor() {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(user);
     if (!user) {
       router.replace("/");
     }
   }, []);
 
   return (
-    <Layout>
-      <TutorComponent />
-    </Layout>
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url('https://plus.unsplash.com/premium_photo-1733306464128-d6b80ed2f2e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+      }}
+    >
+      <div className="bg-white/80 backdrop-blur-md min-h-screen">
+        <Layout>
+          <TutorComponent />
+        </Layout>
+      </div>
+    </div>
   );
 }
