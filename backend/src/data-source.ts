612ec59e5deef009f8580b72d5b8746d3d05ed21
Chakit Bhandari <joshua@filmneverdie.com>
2025-05-19 17:15:27 +1000

    fix: Add entities into datasource

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,9 +1,13 @@
 import "dotenv/config";
 
 import { DataSource } from "typeorm";
-import { User } from "./entities/User";
-import { Course } from "./entities/Course";
-import { Application } from "./entities/Application";
+import { User } from "./entity/User";
+import { Course } from "./entity/Course";
+import { Application } from "./entity/Application";
+import Ranking from "./entity/Ranking";
+import { Experience } from "./entity/Experience";
+import { Role } from "./entity/Role";
+import { Skills } from "./entity/Skills";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -12,7 +16,8 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  // Indicates if database schema should be auto created on every application launch. Should be false in production.
   synchronize: true,
   logging: true,
-  entities: [User, Course, Application],
+  entities: [User, Course, Application, Experience, Ranking, Role, Skills],
 });
