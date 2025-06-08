8806f38d161f6fbab1429ed80334382435f022b7
Chakit Bhandari <joshua@filmneverdie.com>
2025-06-08 15:17:33 +1000

    feat: Define role and course types

--- frontend/src/types/Course.ts
+++ frontend/src/types/Course.ts
@@ -1,4 +1,30 @@
 export interface Course {
-  code: string;
-  label: string;
+  id: string;
+  name: string;
+  course_code: string;
 }
+
+// import { Column, ManyToMany, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+// import { Entity } from "typeorm";
+// import { Application } from "./Application";
+// import { User } from "./User";
+
+// @Entity()
+// export class Course {
+//   @PrimaryGeneratedColumn("uuid")
+//   id: string;
+
+//   @Column()
+//   name: string;
+
+//   @Column()
+//   course_code: string;
+
+//   // A course can have many applications
+//   @OneToMany(() => Application, (application) => application.course)
+//   applications: Application[];
+
+//   // A course can have many lecturers assigned to it
+//   @ManyToMany(() => User, (user) => user.courses)
+//   users: User[];
+// }
