0dde1aed2905839e3e5e8e005f7f43ace70a260b
Chakit Bhandari <joshua@filmneverdie.com>
2025-06-12 05:05:42 +1000

    fix: everythin

---
+++ backend/drop_tables.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS application_skills, application, skill, availability, comment, course, ranking, role, user, experience, typeorm_migrations;
---
+++ backend/src/migration/1749668296314-FixApplicationSkillsTable.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class FixApplicationSkillsTable1749668296314
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
